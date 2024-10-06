a5a47ebaa5f56fbd436028f98028f9589557721d
VannTakashi <andreanrevan276@gmail.com>
2024-10-06 10:19:42 +0700

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -4,11 +4,11 @@ import socket
 import sys
 
 # Telegram API token
-TOKEN = "YOUR TOKEN HERE"
+TOKEN = "7516655980:AAGe3bHlMDKOpZ9HZBET8j52L-SIBy6SCnk"
 
 
 # Admin chat ID
-ADMIN_CHAT_ID = YOUR_ID_HERE
+ADMIN_CHAT_ID = 6227152745
 
 # Initialize bot
 bot = telebot.TeleBot(TOKEN)
